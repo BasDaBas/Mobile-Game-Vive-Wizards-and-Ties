b81558deb9fc231d138090e2aa6c60cd72b66cf4
BasDaBas <Basbenjamins1994@gmail.com>
2017-05-10 20:05:52 +0200

    Added the game in the folder

---
+++ Ongehoord Mobile game/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+    public float autoLoadNextLevelAfter;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        Debug.Log("Dont destroy on load: " + name);
+
+        if (autoLoadNextLevelAfter <= 0)
+        {
+            Debug.Log("Level auto next disabled, Use a positive number in seconds");
+        }
+        else {
+            Invoke("LoadNextLevel", autoLoadNextLevelAfter);
+        }
+    }
+
+    public void LoadLevel(string name){
+		Debug.Log ("New Level load: " + name);
+		SceneManager.LoadScene (name);
+        
+	}	
+
+	public void LoadNextLevel(){
+        Debug.Log("LoadNextLevel Requested");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);        
+	}
+    	
+    public void Quit()
+    {
+        //If we are running in a standalone build of the game
+#if UNITY_STANDALONE
+        //Quit the application
+        Debug.Log("Quit requested");
+        Application.Quit();
+#endif
+
+        //If we are running in the editor
+#if UNITY_EDITOR
+        //Stop playing the scene
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }    
+
+}
---
+++ Ongehoord Mobile game/Assets/Scripts/TouchScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchScript : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update ()
+    {
+      
+	}
+
+    void OnGUI()
+    {
+        foreach (Touch touch in Input.touches)
+        {
+            string message = "";
+            message += "ID: " + touch.fingerId + "\n";
+            message += "Phase: " + touch.phase.ToString() + "\n";
+            message += "TapCount: " + touch.tapCount + "\n";
+            message += "PositionX: " + touch.position.x + "\n";
+            message += "PositionY: " + touch.position.y + "\n";
+
+            int num = touch.fingerId;
+            GUI.Label(new Rect(0 + 130 * num, 0, 120, 100), message);
+        }
+    }
+
+
+}
