c2f4a8156a9858ee2e43e6897080ac0914026e7a
BasDaBas <Basbenjamins1994@gmail.com>
2017-05-23 15:45:39 +0200

    Van Alles, had iets eerder moeten commiten

---
+++ Ongehoord Mobile game/Assets/Scripts/LightController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour {
+
+    public GameObject[] lights;
+
+	// Use this for initialization
+	void Start ()
+    {
+        StartCoroutine(switchThroughLights());
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    IEnumerator switchThroughLights()
+    {
+        
+
+        yield return new WaitForSeconds(1);
+        lights[0].SetActive(false);
+        lights[1].SetActive(true);
+        yield return new WaitForSeconds(1);
+        lights[1].SetActive(false);
+        lights[2].SetActive(true);
+        yield return new WaitForSeconds(1);
+        lights[2].SetActive(false);
+        lights[3].SetActive(true);
+        yield return new WaitForSeconds(1);
+        lights[3].SetActive(false);
+        lights[4].SetActive(true);
+        yield return new WaitForSeconds(1);
+        lights[4].SetActive(false);
+        lights[0].SetActive(true);
+       
+        StartCoroutine(switchThroughLights());
+
+
+
+    }
+}
