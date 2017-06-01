7178c0cffefbc6250aa75762ea44575a44ce2a8b
BasDaBas <Basbenjamins1994@gmail.com>
2017-06-01 02:09:42 +0200

    Unity Analytics and ingame shop

---
+++ Ongehoord Mobile game/Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CompleteProject
+{
+    public class Player : MonoBehaviour
+    {
+        public GameObject shield;
+
+        public ShieldObject[] shields;
+        public int currentShield = 0;
+        
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
---
+++ Ongehoord Mobile game/Assets/Scripts/Shop Scripts/ShieldObject.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class ShieldObject : ScriptableObject {
+
+	public string itemName = "Item Name Here";
+	public int cost = 50;
+	public string description;	
+	public Color color = Color.yellow;
+
+	
+}
